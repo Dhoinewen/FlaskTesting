c08c9c16700e81ea156bc9d7d0a9b756aac67e8d
Maxim <PogChamp1401@gmail.com>
2021-10-01 21:12:30 +0300

    step 2. Now on middle lesson 3

--- main.py
+++ main.py
@@ -1,19 +1,58 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
-app = Flask(__name__)
+main = Flask(__name__)
+main.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+main.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(main)
 
 
-@app.route('/')
-def main():
-    return "hello"
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
-@app.route('/about')
+@main.route('/')
+@main.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@main.route('/about')
 def about():
-    return 'about'
+    return render_template('about.html')
+
+
+@main.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        #print(title)
+        intro = request.form['intro']
+        #print(intro)
+        text = request.form['text']
+        #print(text)
+
+        article = Article(title=title, intro=intro, text=text)
+        #print(article.text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error"
+    else:
+        return render_template('create-article.html')
 
 
 if __name__ == '__main__':
-    app.run( debug = True )
+    main.run( debug = True )
 
